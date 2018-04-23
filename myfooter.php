d655dda2eea42227025874287763f3a92dc925ff
appzminetech <38648445+appzminetech@users.noreply.github.com>
2018-04-23 14:07:48 +0530

    Add files via upload

---
+++ myfooter.php
@@ -0,0 +1,13 @@
+<footer>
+<div class="div_leftf">
+        <a href="#">Home</a>|
+        <a href="#">About</a>|
+        <a href="#">Contact</a>|
+        <a href="#">Log Out</a>
+</div>
+<div class="div_rightf">
+    <p>Designed and developed by Priyadarshini college of engineering.</p>
+</div>
+    </footer>
+</body>
+</html>
---
+++ mymenu.php
@@ -0,0 +1,31 @@
+<html>
+    <head><title>My Menu Bar</title>
+    <link href="loggedinstyle.css" rel="stylesheet"/>
+    </head>
+<body>
+    <header>
+    <div class="navbar">
+        <div class="div_left">
+            <a href="#">Digitalization In Transportation</a>
+        </div>
+        <div class="div_right">
+            <a href="#">My Profile</a>|
+            <a href="#">Add new Vehicle</a>|
+            <a href="#">View Vehicles</a>|
+            <a href="#">Log Out</a>
+        </div>
+    </div>
+<header>
+    <footer>
+<div class="div_leftf">
+        <a href="#">Home</a>|
+        <a href="#">About</a>|
+        <a href="#">Contact</a>|
+        <a href="#">Log Out</a>
+</div>
+<div class="div_rightf">
+    <p>Designed and developed by Priyadarshini college of engineering.</p>
+</div>
+    </footer>
+</body>
+</html>
